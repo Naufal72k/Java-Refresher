6802cc60f077b116ed6c69ee770cb27f23d5d4c5
Naufal72k <naufal72k@gmail.com>
2025-08-22 12:20:40 +0800

    hari pertama

---
+++ belajar/day1.java
@@ -0,0 +1,54 @@
+public class day1 {
+
+    // static void myMethod(String fname, int age) {
+    // System.err.println(fname + " Refsness" + " is age: " + age);
+    // }
+
+    // public static void main(String[] args) {
+    // myMethod("liam", 5);
+    // myMethod("Jenny", 8);
+    // myMethod("Anja", 31);
+
+    // }
+
+    // static void checkAge(int age) {
+    // if (age < 18) {
+    // System.err.println(age + " Acess denied - You are not old enough!");
+    // } else {
+    // System.err.println(age + " Access granted - You are old enough!");
+    // }
+    // }
+
+    // public static void main(String[] args) {
+    // checkAge(10);
+    // checkAge(20);
+    // checkAge(17);
+    // }
+
+    // static int myMethod(int x) {
+    // return 5 + x;
+    // }
+
+    // public static void main(String[] args) {
+    // int a = myMethod(4);
+    // System.out.println(a);
+
+    // System.out.println(myMethod(5));
+    // }
+
+    // static int plusMethodInt(int x, int y) {
+    // return x * y;
+    // }
+
+    // static double plusMethodeDouble(double x, double y) {
+    // return x + y;
+    // }
+
+    // public static void main(String[] args) {
+    // int myNum1 = plusMethodInt(5, 10);
+    // double myNum2 = plusMethodeDouble(5.21312, 7.213123);
+    // System.out.println("int : " + myNum1);
+    // System.out.println("double : " + myNum2);
+    // }
+
+}
