96b157c9fc2f9f9e95b6c636f84a9530bc219ca8
Naufal72k <naufal72k@gmail.com>
2025-08-22 18:24:33 +0800

    comment out unused sum and countdown methods in Recursion class

--- belajar/Recursion.java
+++ belajar/Recursion.java
@@ -12,16 +12,28 @@ public class Recursion {
     // System.out.println("jumlah :" + result);
     // }
 
-    public static int sum(int start, int end) {
-        if (end > start) {
-            return end + sum(start, end - 1);
-        } else {
-            return end;
-        }
-    }
+    // public static int sum(int start, int end) {
+    // if (end > start) {
+    // return end + sum(start, end - 1);
+    // } else {
+    // return end;
+    // }
+    // }
 
-    public static void main(String[] args) {
-        int result = sum(2, 3);
-        System.out.println("jumlah :" + result);
-    }
+    // public static void main(String[] args) {
+    // int result = sum(2, 3);
+    // System.out.println("jumlah :" + result);
+    // }
+
+    // static void coutdown(int n) {
+    // if (n > 0) {
+    // System.out.println(n + "");
+    // coutdown(n - 1);
+    // }
+    // }
+
+    // public static void main(String[] args) {
+    // coutdown(5);
+
+    // }
 }
