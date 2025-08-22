e36fcac16f69ea8ca178cf75da495c96185939c8
Naufal72k <naufal72k@gmail.com>
2025-08-22 18:28:16 +0800

    add factorial method and main function to Recursion class

--- belajar/Recursion.java
+++ belajar/Recursion.java
@@ -36,4 +36,16 @@ public class Recursion {
     // coutdown(5);
 
     // }
+    static int factorial(int n) {
+        if (n > 1) {
+            return n * factorial(n - 1);
+        } else {
+            return 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int result = factorial(5);
+        System.out.println(result);
+    }
 }
