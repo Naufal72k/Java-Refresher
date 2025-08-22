da16b1d82dda189c5684fc0871c10b624e616bb5
Naufal72k <naufal72k@gmail.com>
2025-08-23 06:56:29 +0800

    update instance variable x in Main class from 5 to 10

--- Java_Oop/Main.java
+++ Java_Oop/Main.java
@@ -1,6 +1,15 @@
 package Java_Oop;
 
 public class Main {
-    int x = 5;
+    int x = 10;
+
+    public static void main(String[] args) {
+        Main myObj1 = new Main();
+        Main myObj2 = new Main();
+        myObj2.x = 40;
+
+        System.out.println(myObj1.x);
+        System.out.println(myObj2.x);
+    }
 
 }
