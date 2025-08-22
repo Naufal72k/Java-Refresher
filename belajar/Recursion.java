aa7017637d29df268b3bd6d604ae5979a05fa22f
Naufal72k <naufal72k@gmail.com>
2025-08-22 18:16:39 +0800

    add Recursion class with sum method and main function

---
+++ belajar/Recursion.java
@@ -0,0 +1,27 @@
+public class Recursion {
+    // public static int sum(int k) {
+    // if (k > 0) {
+    // return k + sum(k - 1);
+    // } else {
+    // return 0;
+    // }
+    // }
+
+    // public static void main(String[] args) {
+    // int result = sum(5);
+    // System.out.println("jumlah :" + result);
+    // }
+
+    public static int sum(int start, int end) {
+        if (end > start) {
+            return end + sum(start, end - 1);
+        } else {
+            return end;
+        }
+    }
+
+    public static void main(String[] args) {
+        int result = sum(2, 3);
+        System.out.println("jumlah :" + result);
+    }
+}
