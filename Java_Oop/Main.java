17cc3ab0f24c34376adf461eb41b590d313e3a7e
Naufal72k <naufal72k@gmail.com>
2025-08-23 06:48:54 +0800

    add Main class with main method and instance variable

---
+++ Java_Oop/Main.java
@@ -0,0 +1,11 @@
+package Java_Oop;
+
+public class Main {
+    int x = 5;
+
+    public static void main(String[] args) {
+        Main myObj = new Main();
+        System.out.println(myObj.x);
+
+    }
+}
