fb3be81d14f54fbdf5f6307ccc534172d41bb709
Naufal72k <naufal72k@gmail.com>
2025-08-23 06:49:30 +0800

    refactor main method to create two instances of Main class

--- Java_Oop/Main.java
+++ Java_Oop/Main.java
@@ -4,8 +4,9 @@ public class Main {
     int x = 5;
 
     public static void main(String[] args) {
-        Main myObj = new Main();
-        System.out.println(myObj.x);
-
+        Main myObj1 = new Main();
+        Main myObj2 = new Main();
+        System.out.println(myObj1.x);
+        System.out.println(myObj2.x);
     }
 }
