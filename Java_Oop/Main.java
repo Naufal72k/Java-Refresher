cdcd21356e16d5497102227bd6892a43a0a684f4
Naufal72k <naufal72k@gmail.com>
2025-08-24 23:02:05 +0800

    Class Methods done

--- Java_Oop/Main.java
+++ Java_Oop/Main.java
@@ -27,16 +27,16 @@ public class Main {
     // }
 
     public void fullThrottle() {
-        System.out.println("The car is going as fast as it can!");
+        System.out.println("The car is going as fast as it !");
     }
 
     public void speed(int maxSpeed) {
         System.out.println("Max speed is: " + maxSpeed);
     }
 
-    public static void main(String[] args) {
-        Main myCar = new Main();
-        myCar.fullThrottle();
-        myCar.speed(200);
-    }
+    // public static void main(String[] args) {
+    // Main myCar = new Main();
+    // myCar.fullThrottle();
+    // myCar.speed(200);
+    // }
 }
--- Java_Oop/Second.java
+++ Java_Oop/Second.java
@@ -1,8 +1,9 @@
 package Java_Oop;
 
 public class Second {
-    // public static void main(String[] args) {
-    // Main myObj = new Main();
-    // System.out.println(myObj.x);
-    // }
+    public static void main(String[] args) {
+        Main myCar = new Main();
+        myCar.fullThrottle();
+        myCar.speed(200);
+    }
 }
