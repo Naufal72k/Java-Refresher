9b80950995edeb9b64ffd772cc2416a0f9a49073
Naufal72k <naufal72k@gmail.com>
2025-08-24 19:08:54 +0800

    refactor Main class to comment out instance variables and update main method to call myPublicMethode

--- Java_Oop/Main.java
+++ Java_Oop/Main.java
@@ -1,15 +1,28 @@
 package Java_Oop;
 
 public class Main {
-    String fname = "John";
-    String lname = "Doe";
-    int age = 24;
+    // String fname = "John";
+    // String lname = "Doe";
+    // int age = 24;
+
+    // public static void main(String[] args) {
+    // Main myObj = new Main();
+
+    // System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+    // System.out.println("Age: " + myObj.age);
+    // }
+    static void myStaticMethod() {
+        System.out.println("ini static");
+
+    }
+
+    public void myPublicMethode() {
+        System.out.println("ini public method");
+    }
 
     public static void main(String[] args) {
         Main myObj = new Main();
 
-        System.out.println("Name: " + myObj.fname + " " + myObj.lname);
-        System.out.println("Age: " + myObj.age);
+        myObj.myPublicMethode();
     }
-
 }
