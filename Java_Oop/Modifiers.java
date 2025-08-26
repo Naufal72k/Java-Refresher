cbe10bef2ba45cc91977903c7feb4902de030895
Naufal72k <naufal72k@gmail.com>
2025-08-26 19:40:49 +0800

    Modifiers

---
+++ Java_Oop/Modifiers.java
@@ -0,0 +1,26 @@
+package Java_Oop;
+
+public class Modifiers {
+    final int x = 10;
+    final double pi = 3.14;
+
+    static int myfun() {
+        return 5;
+    }// kalo static void maupun int dia ga perlu buat Main myObj = new Main();, dia
+     // ga perlu , tinggal panggil aja. kalo obj baru perlu misal ke di bawah ni
+
+    public void myPublicMethod() {
+        System.out.println("ini public methode");
+    }
+
+    public static void main(String[] args) {
+        Modifiers myObj = new Modifiers();
+        // int a = myObj.x = 50; // gabakal keganti
+        // myObj.pi = 25;
+        System.out.println(myObj.x);
+        int a = myfun();
+        System.out.println(a);
+        myObj.myPublicMethod();
+
+    }
+}
