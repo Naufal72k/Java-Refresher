6bc9edce60e8ae496d39279a499c3ae606e825ff
Naufal72k <naufal72k@gmail.com>
2025-08-28 10:34:03 +0800

    Add Encapsulation class with Person inner class and getter method

---
+++ Java_Oop/Encapsulation.java
@@ -0,0 +1,16 @@
+package Java_Oop;
+
+public class Encapsulation {
+    public class Person {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
