17f97a70ee50aa1fdc6be663e37fb1816fdd7932
Naufal72k <naufal72k@gmail.com>
2025-09-03 22:44:13 +0800

    Linked List N Bubble sort

---
+++ simpleProject/LinkedList.java
@@ -0,0 +1,65 @@
+package simpleProject;
+
+public class LinkedList {
+    Node head;
+
+    LinkedList() {
+        this.head = null;
+    }
+
+    // Add node at the end
+    void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    // Get length of linked list
+    int getLength() {
+        int length = 0;
+        Node current = head;
+        while (current != null) {
+            length++;
+            current = current.next;
+        }
+        return length;
+    }
+
+    // Bubble sort the linked list
+    void bubbleSort() {
+        if (head == null)
+            return;
+
+        boolean swapped;
+        do {
+            swapped = false;
+            Node current = head;
+            while (current.next != null) {
+                if (current.data > current.next.data) {
+                    int temp = current.data;
+                    current.data = current.next.data;
+                    current.next.data = temp;
+                    swapped = true;
+                }
+                current = current.next;
+            }
+        } while (swapped);
+    }
+
+    // Print the linked list
+    void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
