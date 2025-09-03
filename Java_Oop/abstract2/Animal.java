5aa75e3ea7f657826c452526eb85d4ba60a982ad
Naufal72k <naufal72k@gmail.com>
2025-09-03 21:26:32 +0800

    abstract

---
+++ Java_Oop/abstract2/Animal.java
@@ -0,0 +1,24 @@
+package Java_Oop.abstract2;
+
+abstract class Animal {
+    public abstract void animalSound();
+
+    public void sleep() {
+        System.out.println("zzz");
+    }
+}
+// Animal myObj = new Animal(); // gabisa kita langsung manggil kek obj biasanya
+
+class Pig extends Animal {
+    public void animalSound() {
+        System.out.println("the pig says: weeeeee");
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Pig myPig = new Pig();
+        myPig.animalSound();
+        myPig.sleep();
+    }
+}
