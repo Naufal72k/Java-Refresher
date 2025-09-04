bab96bb1dae49336305546ea24efca8f3a666c4d
Naufal72k <naufal72k@gmail.com>
2025-09-04 22:09:35 +0800

    belajar linkedlist

---
+++ belajarLinkedList/LinkedList.java
@@ -0,0 +1,19 @@
+package belajarLinkedList;
+
+public class LinkedList {
+    private Node head;
+
+    public LinkedList() {
+        this.head = null;
+    }
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+
+        }
+    }
+}
