90f33f5665c4deab5718f037addb143db2f6a338
Naufal72k <naufal72k@gmail.com>
2025-09-09 08:49:23 +0800

    sorting

--- ALGODAT/bubbleSort/Sorting.java
+++ ALGODAT/bubbleSort/Sorting.java
@@ -3,59 +3,81 @@ package bubbleSort;
 public class Sorting {
     Node head;
 
-    public Sorting() {
-        this.head = null;
-    }
-
     public void add(int data) {
         Node newNode = new Node(data);
-
         if (head == null) {
             head = newNode;
-        } else {
-            Node temp = head;
-            while (temp.next != null) {
-                temp = temp.next;
-
-            }
-            temp.next = newNode;
+            return;
         }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
     }
 
     public void display() {
-        Node temp = head;
-        while (temp != null) {
-            System.out.println(temp.data);
-            temp = temp.next;
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
         }
-
+        System.out.println();
     }
 
     public void bubbleSort() {
-        if (head == null) {
-            System.out.println("blom ada apa apa");
-
-        } else {
-            boolean swap;
-
-            do {
-                swap = false;
-                Node temp = head;
-
-                while (temp.next != null) {
-                    if (temp.data > temp.next.data) {
-                        int simpan = temp.data;
-                        temp.data = temp.next.data;
-                        temp.next.data = simpan;
-                        swap = true;
-
-                    }
-                    temp = temp.next;
-
+        if (head == null)
+            return;
+        boolean swapped;
+        do {
+            swapped = false;
+            Node current = head;
+            while (current.next != null) {
+                if (current.data > current.next.data) {
+                    int temp = current.data;
+                    current.data = current.next.data;
+                    current.next.data = temp;
+                    swapped = true;
                 }
+                current = current.next;
+            }
+        } while (swapped);
+    }
 
-            } while (swap);
+    public void selectionSort() {
+        for (Node current = head; current != null; current = current.next) {
+            Node min = current;
+            for (Node next = current.next; next != null; next = next.next) {
+                if (next.data < min.data) {
+                    min = next;
+                }
+            }
+            int temp = current.data;
+            current.data = min.data;
+            min.data = temp;
         }
     }
 
+    public void insertionSort() {
+        if (head == null || head.next == null)
+            return;
+        Node sorted = null;
+        Node current = head;
+        while (current != null) {
+            Node next = current.next;
+            if (sorted == null || current.data < sorted.data) {
+                current.next = sorted;
+                sorted = current;
+            } else {
+                Node temp = sorted;
+                while (temp.next != null && temp.next.data < current.data) {
+                    temp = temp.next;
+                }
+                current.next = temp.next;
+                temp.next = current;
+            }
+            current = next;
+        }
+        head = sorted;
+    }
 }
