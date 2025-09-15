2bb4ab3181ab77fe7a11323abef062e66fb1d918
Naufal72k <naufal72k@gmail.com>
2025-09-16 07:56:07 +0800

    Test 1

---
+++ PBO/pertemuanArray/Main.java
@@ -0,0 +1,10 @@
+package pertemuanArray;
+
+public class Main {
+    public static void main(String[] args) {
+        String s1 = new String("Java");
+        String s2 = new String("Java");
+        System.out.println("s1 == s2" + (s1 == s2));
+        System.out.println("s1 == s2" + s1.equals(s2));
+    }
+}
