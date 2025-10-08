050f30f978f1c4756ba2c554caa3692df4dc7e47
Naufal72k <naufal72k@gmail.com>
2025-10-08 14:28:07 +0800

    ngetes

--- ALGODAT/Modul 1/LinkedList.java
+++ ALGODAT/Modul 1/LinkedList.java
@@ -1,6 +1,20 @@
 public class LinkedList {
-    Node Legends;
-    Node currentPlayer;
-    Node youngTalent;
+    Node LegendsHead;
+    Node LegendsTail;
+    int idxLegends = 0;
 
+    public void addLegendsAwal(String playerName, String position, String specialAbility, String skillPower,
+            String skillSpeed, String skillStamina, float overallRating) {
+        Node newLegends = new Node(playerName, position, specialAbility, skillPower, skillSpeed, skillStamina,
+                overallRating);
+
+        if (LegendsHead == null) {
+            LegendsHead = newLegends;
+            LegendsTail = newLegends;
+            idxLegends++;
+        } else {
+
+        }
+
+    }
 }
---
+++ PBO/modul1/Karyawan.java
@@ -0,0 +1,59 @@
+public class Karyawan {
+    private String nama;
+    private String status;
+    private double gajiPerJam;
+    private int jamKerja;
+    private int jlhLembur;
+    private int jlhAbsen;
+    private double bonusLembur;
+    private double potonganAbsen;
+
+    public Karyawan(String nama, String status, double gajiPerJam, int jamKerja, int jlhLembur, int jlhAbsen) {
+        this.nama = nama;
+        this.status = status;
+        this.gajiPerJam = gajiPerJam;
+        this.jamKerja = jamKerja;
+        this.jlhLembur = jlhLembur;
+        this.jlhAbsen = jlhAbsen;
+    }
+
+    public double hitungGaji(int jamKerja) {
+        return jamKerja;
+    }
+
+    public double hitungLembur(int jlhLembur) {
+        return jlhLembur;
+    }
+
+    public double hitungAbsen(int jumlahAbsen) {
+        return jumlahAbsen;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public int getJamKerja() {
+        return jamKerja;
+    }
+
+    public int getJlhLembur() {
+        return jlhLembur;
+    }
+
+    public int getJlmAbsen() {
+        return jlhAbsen;
+    }
+
+    public double getBonusLembur() {
+        return bonusLembur;
+    }
+
+    public double getPotonganAbsen() {
+        return potonganAbsen;
+    }
+}
