11478898002ad3346be80acec099b51eee344989
Naufal72k <naufal72k@gmail.com>
2025-10-08 15:09:40 +0800

    done class 2

--- PBO/modul1/KaryawanTetap.java
+++ PBO/modul1/KaryawanTetap.java
@@ -1,3 +1,14 @@
-public class KaryawanTetap {
+public class KaryawanTetap extends Karyawan {
+    public KaryawanTetap(String nama, int jamKerja, int jlhLembur, int jlhAbsen) {
+        super(nama, "tetap", 2_500_000, jamKerja, jlhLembur, jlhAbsen);
+    }
 
+    public double hitungLembur(int jlhLembur) {
+        return jlhLembur;
+    }
+
+    public double hitungAbsen(int jlhAbsen) {
+
+        return jlhAbsen;
+    }
 }
