d0eb05e0d8409fb20073f9b927e2f76376cb63bc
Naufal72k <naufal72k@gmail.com>
2025-10-28 08:36:24 +0800

    sampe event

--- PBO/GUI/HelloGui.java
+++ PBO/GUI/HelloGui.java
@@ -1,10 +1,26 @@
+import java.lang.classfile.Label;
+
 import javax.swing.*;
 
 public class HelloGui {
     public static void main(String[] args) {
         JFrame frame = new JFrame("contoh GUI");
-        frame.setSize(300, 150);
+        frame.setSize(300, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel label = new JLabel("masukan input");
+        JButton button = new JButton("pencet saya");
+
+        frame.add(label);
+        frame.add(button);
+        button.addActionListener(e -> {
+            JDialog dialog = new JDialog();
+            dialog.setTitle("hello");
+            dialog.setSize(200, 200);
+            dialog.setLocationRelativeTo(frame);
+            dialog.setVisible(true);
+        });
         frame.setVisible(true);
+
     }
 }
