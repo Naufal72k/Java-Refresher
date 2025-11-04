bffcac73eed22961378d5ce6ccfd44be2cf00944
Naufal72k <naufal72k@gmail.com>
2025-11-04 09:10:31 +0800

    coba bntr

---
+++ PBO/GUI/Jurnal.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Jurnal {
+    public static void main(String[] args) {
+        JFrame frameUtama = new JFrame();
+        frameUtama.setSize(800, 800);
+        frameUtama.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frameUtama.setMinimumSize(new Dimension(800, 600));
+        frameUtama.setVisible(true);
+    }
+}
