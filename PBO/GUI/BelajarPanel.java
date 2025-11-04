129a1981307cd317c96a5a6a9dcbecffcc1241b8
Naufal72k <naufal72k@gmail.com>
2025-11-04 08:41:36 +0800

    coba lagi

--- PBO/GUI/BelajarPanel.java
+++ PBO/GUI/BelajarPanel.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Label;
 import java.awt.Panel;
 
 import javax.swing.*;
@@ -19,6 +20,17 @@ public class BelajarPanel {
 
         JPanel panelUtama = new JPanel();
         panelUtama.setBackground(Color.BLACK);
+
+        panelUtama.setLayout(null);
+        JLabel labelJudul = new JLabel("Perpustakaan");
+        labelJudul.setForeground(Color.WHITE);
+        labelJudul.setBounds(350, 50, 200, 30);
+        ImageIcon img = new ImageIcon(getClass().getResource("/image.png"));
+        JLabel image = new JLabel(img);
+
+        image.setBounds(300, 100, 300, 300);
+        panelUtama.add(image);
+
         frameUtama.setContentPane(panelUtama);
         frameUtama.add(panelUtama);
     }
